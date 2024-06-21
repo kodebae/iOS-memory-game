faab684d5292e87caf661c6ad141e952ea80d994
kodebae <heykarmendurbin@gmail.com>
2024-06-20 21:34:17 -0400

    initial commit

---
+++ Memowise/Memowise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memowise
+//
+//  Created by Karmen Durbin on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memowise/Memowise/MemowiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemowiseApp.swift
+//  Memowise
+//
+//  Created by Karmen Durbin on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MemowiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
